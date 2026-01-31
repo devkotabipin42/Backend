3b9ab550316aa4eb9f02bbbaad0818e9251b7ba6
devkotabipin42 <devkotabipin42@gmail.com>
2026-01-31 20:35:38 +0900

    done day 7

---
+++ Day7/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const noteModel =require('./models/notes.model')
+const app = express()
+app.use(express.json())
+/**
+ post/notes
+ req.body=>{tittle,description}
+ */
+
+ app.post('/notes',async(req,res)=>{
+  const {title,description} = req.body
+  const note = await noteModel.create({
+    title,description
+  })
+  res.status(201).json({
+    message:'note created sucessfull',
+    note
+  })
+ })
+
+module.exports = app
