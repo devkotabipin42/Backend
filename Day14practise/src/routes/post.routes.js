8fbbc0e04b42d9a6f1d2b9388c1a978ea3c94da8
devkotabipin42 <devkotabipin42@gmail.com>
2026-02-13 12:13:28 +0900

    update verify token

---
+++ Day14practise/src/routes/post.routes.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const postRouter = express.Router()
+const PostController = require('../controllers/post.controller')
+const multer = require('multer')
+const upload = multer({ storage: multer.memoryStorage() })
+
+
+
+
+postRouter.post('/',upload.single('image'),PostController.createPostController)
+// install multer then
+
+
+module.exports=postRouter
