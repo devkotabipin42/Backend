1e141d06b7cff02ceec074cc4d0b041f3968b3fa
devkotabipin42 <devkotabipin42@.com>
2026-03-30 10:45:03 +0900

    Add new dependencies for Langchain and Pinecone integration

--- Rag/main.js
+++ Rag/main.js
@@ -1,4 +1,8 @@
 import {PDFParse} from 'pdf-parse';
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import {MistralAIEmbeddings} from "@langchain/mistralai"
+import dotenv from 'dotenv';
+dotenv.config();
 import fs from 'fs';
 
 let dataBuffer = fs.readFileSync('./STORY.pdf');
@@ -10,3 +14,22 @@ const parser = new  PDFParse({
 const data = await parser.getText()
 console.log(data);
 
+const embeddings = new MistralAIEmbeddings({
+    apiKey: process.env.MISTRALAI_API_KEY,
+    model:"mistral-embed"
+})
+
+const splitter = new RecursiveCharacterTextSplitter({
+     chunkSize: 400,
+     chunkOverlap: 0
+     })
+const chunks = await splitter.splitText(data.text)
+
+const docs = await Promise.all(chunks.map(async(chunk)=>{
+    const embedding = await embeddings.embedQuery(chunk)
+    return {
+        text: chunk,
+        embedding: embedding
+    }
+}))
+console.log(docs)
